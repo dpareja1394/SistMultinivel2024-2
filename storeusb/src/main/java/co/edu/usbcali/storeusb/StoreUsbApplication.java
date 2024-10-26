b3408e488927f6e17ed09ea36fabb79e873d71be
Daniel Pareja Londoño <dpareja1394@gmail.com>
2024-10-26 10:45:35 -0500

    Creación de Microservicio con SpringBoot

---
+++ storeusb/src/main/java/co/edu/usbcali/storeusb/StoreUsbApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.storeusb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreUsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreUsbApplication.class, args);
+	}
+
+}
---
+++ storeusb/src/test/java/co/edu/usbcali/storeusb/StoreUsbApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.storeusb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreUsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
