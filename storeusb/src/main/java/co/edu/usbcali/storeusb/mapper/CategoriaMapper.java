7580c4ebe9a2b8b5bce59524f26e6104bcdfad39
Daniel Pareja Londoño <dpareja1394@gmail.com>
2024-11-01 18:43:29 -0500

    Mappers con Listas y Objetos

---
+++ storeusb/src/main/java/co/edu/usbcali/storeusb/mapper/CategoriaMapper.java
@@ -0,0 +1,39 @@
+package co.edu.usbcali.storeusb.mapper;
+
+import co.edu.usbcali.storeusb.domain.Categoria;
+import co.edu.usbcali.storeusb.dto.CategoriaDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriaMapper {
+
+    public static CategoriaDTO domainToDTO(Categoria categoria) {
+        return CategoriaDTO.builder()
+                .id(categoria.getId())
+                .nombre(categoria.getNombre())
+                .descripcion(categoria.getDescripcion())
+                .estado(categoria.getEstado())
+                .build();
+
+    }
+
+    public static Categoria dTOToDomain(CategoriaDTO categoriaDTO) {
+        return Categoria.builder()
+                .id(categoriaDTO.getId())
+                .nombre(categoriaDTO.getNombre())
+                .descripcion(categoriaDTO.getDescripcion())
+                .estado(categoriaDTO.getEstado())
+                .build();
+    }
+
+    public static List<CategoriaDTO> domainToDTOList(List<Categoria> categorias) {
+        return categorias.stream().map(CategoriaMapper::domainToDTO).toList();
+    }
+
+    public static List<Categoria> dTOToDomainList(List<CategoriaDTO> categoriasDTO) {
+        return categoriasDTO.stream().map(CategoriaMapper::dTOToDomain).toList();
+    }
+
+
+}
