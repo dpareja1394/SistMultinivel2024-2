6290d56feeb3003a0a3744119f118f782d4322aa
Daniel Pareja Londoño <dpareja1394@gmail.com>
2024-11-01 20:00:18 -0500

    Mappers entidades dependientes

---
+++ storeusb/src/main/java/co/edu/usbcali/storeusb/mapper/ProductoMapper.java
@@ -0,0 +1,42 @@
+package co.edu.usbcali.storeusb.mapper;
+
+import java.util.List;
+
+import co.edu.usbcali.storeusb.domain.Producto;
+import co.edu.usbcali.storeusb.dto.ProductoDTO;
+
+public class ProductoMapper {
+    public static ProductoDTO domainToDTO(Producto producto) {
+        return ProductoDTO.builder()
+        .id(producto.getId())
+        .categoriaId(producto.getCategoria() == null ? null :
+            producto.getCategoria().getId())
+        .referencia(producto.getReferencia())
+        .nombre(producto.getNombre())
+        .descripcion(producto.getDescripcion())
+        .precioUnitario(producto.getPrecioUnitario())
+        .unidadesDisponibles(producto.getUnidadesDisponibles())
+        .estado(producto.getEstado())
+        .build();
+    }
+
+    public static Producto dTOToDomain(ProductoDTO productoDTO) {
+        return Producto.builder()
+        .id(productoDTO.getId())
+        .referencia(productoDTO.getReferencia())
+        .nombre(productoDTO.getNombre())
+        .descripcion(productoDTO.getDescripcion())
+        .precioUnitario(productoDTO.getPrecioUnitario())
+        .unidadesDisponibles(productoDTO.getUnidadesDisponibles())
+        .estado(productoDTO.getEstado())
+        .build();
+    }
+
+    public static List<ProductoDTO> domainToDTOList(List<Producto> productos) {
+        return productos.stream().map(ProductoMapper::domainToDTO).toList();
+    }
+
+    public static List<Producto> dTOToDomainList(List<ProductoDTO> productosDTO) {
+        return productosDTO.stream().map(ProductoMapper::dTOToDomain).toList();
+    }
+}
