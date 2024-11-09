03ea4385581d653024ed5103bf12ca84f227fc83
Daniel Pareja Londoño <dpareja1394@gmail.com>
2024-11-09 09:36:03 -0500

    Anotaciones Transactional

--- storeusb/src/main/java/co/edu/usbcali/storeusb/service/impl/CategoriaServiceImpl.java
+++ storeusb/src/main/java/co/edu/usbcali/storeusb/service/impl/CategoriaServiceImpl.java
@@ -1,7 +1,6 @@
 package co.edu.usbcali.storeusb.service.impl;
 
 import co.edu.usbcali.storeusb.domain.Categoria;
-import co.edu.usbcali.storeusb.domain.Producto;
 import co.edu.usbcali.storeusb.dto.CategoriaDTO;
 import co.edu.usbcali.storeusb.dto.request.CreateCategoriaRequest;
 import co.edu.usbcali.storeusb.dto.response.CategoriaConProductos;
@@ -9,14 +8,14 @@ import co.edu.usbcali.storeusb.dto.response.ProductoResponseCategoriaConProducto
 import co.edu.usbcali.storeusb.mapper.CategoriaMapper;
 import co.edu.usbcali.storeusb.mapper.ProductoMapper;
 import co.edu.usbcali.storeusb.repository.CategoriaRepository;
-import co.edu.usbcali.storeusb.repository.ProductoRepository;
 import co.edu.usbcali.storeusb.service.CategoriaService;
 import co.edu.usbcali.storeusb.utils.Constants;
 import co.edu.usbcali.storeusb.utils.validation.CategoriaMessage;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
@@ -28,6 +27,7 @@ public class CategoriaServiceImpl implements CategoriaService {
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public CategoriaDTO crearCategoria(CreateCategoriaRequest createCategoriaRequest) throws Exception {
 
         // Validar si ya existe una Categoria con el nombre
@@ -48,21 +48,23 @@ public class CategoriaServiceImpl implements CategoriaService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public CategoriaConProductos consultarCategoriaConProductos(Integer categoriaId) throws Exception {
-        // Consultar la categoría
-        Optional<Categoria> categoriaOptional =
-                categoriaRepository.findById(categoriaId);
-        // Si no encuentra la Categoria se indica que existe
-        if(categoriaOptional.isEmpty()) {
-            throw new Exception(String.format(CategoriaMessage.NO_EXISTE_CATEGORIA_POR_ID, categoriaId));
-        }
+        // Consultar la categoría y si no la encuentra, lanza excepcion
+        Categoria categoria =
+                categoriaRepository.findById(categoriaId)
+                        .orElseThrow(
+                                () -> new Exception(
+                                        String.format(CategoriaMessage.NO_EXISTE_CATEGORIA_POR_ID, categoriaId)
+                                )
+                        );
 
         // Mapear la respuesta de Categoría con los Productos
         List<ProductoResponseCategoriaConProductos> productosResponse =
                 ProductoMapper.domainToProductoResponseCategoriaConProductosList(
-                        categoriaOptional.get().getProductos());
+                        categoria.getProductos());
 
-        return CategoriaMapper.domainToCategoriaConProductos(categoriaOptional.get(),
+        return CategoriaMapper.domainToCategoriaConProductos(categoria,
                 productosResponse);
     }
 
--- storeusb/src/main/java/co/edu/usbcali/storeusb/service/impl/ProductoServiceImpl.java
+++ storeusb/src/main/java/co/edu/usbcali/storeusb/service/impl/ProductoServiceImpl.java
@@ -8,7 +8,10 @@ import co.edu.usbcali.storeusb.mapper.ProductoMapper;
 import co.edu.usbcali.storeusb.repository.CategoriaRepository;
 import co.edu.usbcali.storeusb.repository.ProductoRepository;
 import co.edu.usbcali.storeusb.service.ProductoService;
+import co.edu.usbcali.storeusb.utils.validation.CategoriaMessage;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -24,24 +27,25 @@ public class ProductoServiceImpl implements ProductoService {
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public ProductoDTO crearProducto(CreateProductoRequest createProductoRequest) throws Exception {
         // Validacion de los campos
         if (createProductoRequest == null) {
             throw new Exception("No ha llegado el Objeto Producto para crear");
         }
-        if(createProductoRequest.getReferencia() == null ||
-        createProductoRequest.getReferencia().trim().equals("")){
+        if (createProductoRequest.getReferencia() == null ||
+                createProductoRequest.getReferencia().trim().equals("")) {
             throw new Exception("La referencia no puede ser nula ni vacía");
         }
-        if(createProductoRequest.getReferencia().length()>10) {
+        if (createProductoRequest.getReferencia().length() > 10) {
             throw new Exception("La referencia no puede superar el tamaño de 10 caracteres");
         }
 
-        if(createProductoRequest.getNombre() == null ||
-                createProductoRequest.getNombre().trim().equals("")){
+        if (createProductoRequest.getNombre() == null ||
+                createProductoRequest.getNombre().trim().equals("")) {
             throw new Exception("El nombre no puede ser nulo ni vacío");
         }
-        if(createProductoRequest.getNombre().length()>50) {
+        if (createProductoRequest.getNombre().length() > 50) {
             throw new Exception("El nombre no puede superar el tamaño de 50 caracteres");
         }
 
@@ -50,24 +54,23 @@ public class ProductoServiceImpl implements ProductoService {
             throw new Exception("La descripción de la Categoría no puede exceder los 255 caracteres");
         }
 
-        if (createProductoRequest.getPrecioUnitario() == null ){
+        if (createProductoRequest.getPrecioUnitario() == null) {
             throw new Exception("El precio unitario no puede ser nulo");
         }
 
-        if (createProductoRequest.getUnidadesDisponibles() == null ){
+        if (createProductoRequest.getUnidadesDisponibles() == null) {
             throw new Exception("Las unidades disponibles no puede ser nulas");
         }
 
         // Validar que la categoría exista
-        Optional<Categoria> categoriaOptional =
-                categoriaRepository.findById(createProductoRequest.getCategoriaId());
-
-        if (categoriaOptional.isEmpty()) {
-            throw new Exception("La categoría con id "+createProductoRequest.getCategoriaId()+" no existe");
-        }
+        Categoria categoria = categoriaRepository.findById(createProductoRequest.getCategoriaId())
+                .orElseThrow(
+                        () -> new Exception(String.format(CategoriaMessage.NO_EXISTE_CATEGORIA_POR_ID,
+                                createProductoRequest.getCategoriaId()))
+                );
 
         Producto producto = ProductoMapper.createProductoRequestToDomain(createProductoRequest);
-        producto.setCategoria(categoriaOptional.get());
+        producto.setCategoria(categoria);
 
         producto = productoRepository.save(producto);
 
